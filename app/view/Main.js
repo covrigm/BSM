b7674244b99b2b12314dd316b00ed4768ebf1bb6
Mark Covrig <covrigm@hotmail.com>
2012-06-19 19:43:19 -0500

    2nd test
    2nd test desc

--- app/view/Main.js
+++ app/view/Main.js
@@ -18,22 +18,24 @@ Ext.define("GS.view.Main", {
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Dell Rapids Area News'
+                    title: 'Welcome to Sencha Touch 2'
                 },
 
                 html: [
-                    "Build something!"
+                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
                 ].join("")
             },
             {
-                title: 'News',
+                title: 'Get Started',
                 iconCls: 'action',
 
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'News'
+                        title: 'Getting Started'
                     },
                     {
                         xtype: 'video',
